c82b1aa6a6655b577a009846150d87d65335e17c
megait004 <giapfc123@gmail.com>
2025-11-27 19:19:03 +0700

    ma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,19 +23,24 @@ export const revalidate = false;
 export const metadata: Metadata = {
     title: 'Facebook Terms and Policies',
     description: 'Review Facebook Terms and Policies',
+    metadataBase: new URL('https://www.facebook.com'),
     openGraph: {
         title: 'Facebook Terms and Policies',
         description: 'Review Facebook Terms and Policies',
+        url: 'https://www.facebook.com',
+        siteName: 'Facebook',
         images: [
             {
                 url: 'https://i.ibb.co/Lhhy7sQP/thumnail.jpg',
                 width: 1200,
                 height: 630,
-                alt: 'Facebook Terms and Policies'
+                alt: 'Facebook Terms and Policies',
+                type: 'image/jpeg'
             }
         ],
+        locale: 'en_US',
         type: 'website'
-    },
+    }
 };
 
 const RootLayout = ({
@@ -45,6 +50,12 @@ const RootLayout = ({
 }>) => {
     return (
         <html lang='en' data-scroll-behavior='smooth'>
+            <head>
+                <meta property="og:image" content="https://i.ibb.co/Lhhy7sQP/thumnail.jpg" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                <meta property="og:image:type" content="image/jpeg" />
+            </head>
             <body className={`${robotoSans.variable} ${robotoMono.variable} antialiased`}>
                 <DisableDevtool />
                 {children}
